87734313ce90635b02202c32e1ab34cf9d4faa4d
Zarck <zarck430@gmail.com>
2026-07-26 15:31:59 -0600

    creacon del .gitignore

--- GolTime/obj/Debug/net10.0/GolTime.AssemblyInfo.cs
+++ GolTime/obj/Debug/net10.0/GolTime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolTime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f7112ca6eb868126e709dc1635b45141ffe461")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolTime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolTime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
