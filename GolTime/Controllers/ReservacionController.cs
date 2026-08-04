f5dd97b844b8bfe3bdcda01ef3bb3ca6485bb9ea
Zarck <zarck430@gmail.com>
2026-08-03 18:04:51 -0600

    Vistas y nuevo paquete de questpdf

---
+++ GolTime/Controllers/ReservacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GolTime.Controllers
+{
+    public class ReservacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
